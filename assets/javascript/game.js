72ec94eb5fae63f8350de0dcebad9f84a23ce5c9
Alison Kelly <alisonbv@gmail.com>
2019-07-03 18:11:52 -0400

    added final css styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,18 +12,13 @@ var losses = 0;
 var counter= 0;
 
 var num1= Math.floor(Math.random()*3+1)
-console.log(num1);
 var num2= Math.floor(Math.random()*6+1)
-console.log(num2);
 var num3= Math.floor(Math.random()*6+1) + 6;
-console.log(num3);
 var num4= Math.floor(Math.random()*6+1) +6;
-console.log(num4);
 
 // <!-- Create Reset for the game after the user wins or goes over the Random Total Number * The random number shown at the start of the game should be between 19 - 120. -->
 
 function reset() {
-    console.log("Game Over");
     targetNumber = Math.floor(Math.random() * 120) + 19;
 
     $("#pathWeight").text(targetNumber);
@@ -42,28 +37,24 @@ $("#one").on("click", function() {
     counter += num1;
     var crystalValue = ($(this).attr("data-crystalvalue"));
     crystalValue = parseInt(crystalValue);
-    console.log(counter);
 });
 
 $("#two").on("click", function() {
     counter += num2;
     var crystalValue = ($(this).attr("data-crystalvalue"));
     crystalValue = parseInt(crystalValue);
-    console.log(counter);
 });
 
 $("#three").on("click", function() {
     counter += num3;
     var crystalValue = ($(this).attr("data-crystalvalue"));
     crystalValue = parseInt(crystalValue);
-    console.log(counter);
 });
 
 $("#four").on("click", function() {
     counter += num4;
     var crystalValue = ($(this).attr("data-crystalvalue"));
     crystalValue = parseInt(crystalValue);
-    console.log(counter);
 });
 
 
@@ -75,13 +66,15 @@ $("#four").on("click", function() {
 
     if (counter === targetNumber) {
       alert("You have brought wealth to your Clan!");
-      $("#wins-text").text(wins= wins +1 + " Won");
+      $("#wins-text").text(wins= 1+ wins + " Won");
+      wins = parseInt(wins);
       reset();
     }
 // <!-- Have an alert if they lose that says: "You dug too deep!"
     else if (counter >= targetNumber) {
       alert("You dug too deep!");
-      $("#losses-text").text(losses= losses +1 + " Lost");
+      $("#losses-text").text(losses= 1+ losses + " Lost");
+      losses = parseInt(losses);
       reset();
     }
 
